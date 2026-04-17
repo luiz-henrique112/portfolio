f1ebb9dda08b44603ca2d968c6618290631ae8a0
luiz-henrique112 <108096881+luiz-henrique112@users.noreply.github.com>
2026-04-16 22:31:25 -0300

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/' // 👈 nome do seu repositório
+  base: 'portfolio' // 👈 nome do seu repositório
 })
